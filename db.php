3ca7df9e17eddfc25ffa61b25a564289a7a6a243
Jonathan Dean <ke4ukz@users.noreply.github.com>
2026-01-02 21:28:13 -0500

    Reapply "db connection test"
    This reverts commit 48e773b3d75222c1069c45822301780c531faea8.

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$GLOBALS['db'] = FALSE;
+if ($GLOBALS['config'] !== FALSE) {
+    try {
+        $GLOBALS['db'] = new PDO("mysql:{$GLOBALS['config']['database']['host']}=localhost;dbname={$GLOBALS['config']['database']['dbname']}", $GLOBALS['config']['database']['username'], $GLOBALS['config']['database']['password']);
+    } catch (PDOException $e) {
+        $GLOBALS['db'] = FALSE;
+    }
+}
+
+?>
