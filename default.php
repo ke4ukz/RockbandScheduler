c106ac0150d0f6551a00f88ed9fa4e0a51d40093
Jonathan Dean <ke4ukz@users.noreply.github.com>
2026-01-02 23:44:05 -0500

    db test

--- default.php
+++ default.php
@@ -13,6 +13,14 @@ echo($GLOBALS['config']);
 require_once 'db.php';
 echo($GLOBALS['db']);
 
+$db = $GLOBALS['db'];
+$stmt = $db->query('DESCRIBE songs;');
+
+foreach ($stmt as $row) {
+    print_r($row);
+}
+
+
 ?>
     </body>
     done?
