e946bb0db1f76bdbb2d3842ad2bcf58059445901
Jonathan Dean <ke4ukz@users.noreply.github.com>
2026-01-02 20:19:25 -0500

    load config test

--- default.php
+++ default.php
@@ -5,8 +5,23 @@
     </head>
     <body>
 <?php
-echo("Script worked");
+// Ensure this file is outside your web root!
+$configPath = __DIR__ . '../../config/rockband_scheduler_config.ini'; // Adjust path as needed
 
+file_exists($configPath) || die("Configuration file not found.");
+
+$dbSettings = parse_ini_file($configPath, true);
+
+if (!$dbSettings) {
+    die("Failed to parse configuration file.");
+}
+
+$dbHost = $dbSettings['database']['host'];
+$dbName = $dbSettings['database']['dbname'];
+$dbUser = $dbSettings['database']['username'];
+$dbPass = $dbSettings['database']['password'];
+
+echo ("config loaded");
 ?>
     </body>
 </html>
